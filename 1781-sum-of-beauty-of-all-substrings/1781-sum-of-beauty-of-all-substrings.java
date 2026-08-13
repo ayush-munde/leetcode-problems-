3825cca1d6c85425367cfcac21ee7aaf3a70dc4b
Ayush Munde <ayushmunde2006@gmail.com>
2026-08-13 21:22:14 +0530

    Time: 497 ms (29.51%), Space: 46.8 MB (9.64%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int beautySum(String s) {
+        int n = s.length();
+        int sum = 0;
+
+        // Loop through all substrings
+        for (int i = 0; i < n; i++) {
+            Map<Character, Integer> freq = new HashMap<>();
+
+            for (int j = i; j < n; j++) {
+                // Increase character frequency
+                freq.put(s.charAt(j), freq.getOrDefault(s.charAt(j), 0) + 1);
+
+                int maxi = Integer.MIN_VALUE;
+                int mini = Integer.MAX_VALUE;
+
+                // Calculate max and min frequency
+                for (int val : freq.values()) {
+                    mini = Math.min(mini, val);
+                    maxi = Math.max(maxi, val);
+                }
+
+                // Add to sum
+                sum += (maxi - mini);
+            }
+        }
+
+        return sum;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "xyx";
+        System.out.println("Beauty Sum: " + sol.beautySum(s));
+    }
+}
